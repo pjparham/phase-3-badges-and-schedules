b6b9f62e9a1c4fcccf7c9d8805435b95ab52d30d
Parker Parham <parkerparham@gmail.com>
2022-10-14 12:18:39 -0400

    commit tree dc09257ae437f52d6fc7aa337863e86e5b3b5539

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index += 1}!"
+    end
+end
+
+def printer (speakers)
+    speakers.each {|speaker| puts badge_maker(speaker)}
+    assign_rooms(speakers).each{|message| puts message}
+end
+
